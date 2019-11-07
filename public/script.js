fd2eb2b47b7a3f5108e155cbf3ebcda9961f1bb6
spencerjchavez <spencerjchavez@yahoo.com>
2019-11-07 23:05:48 +0000

    added seperate inputs for dates

--- public/script.js
+++ public/script.js
@@ -7,6 +7,10 @@ var app = new Vue({
         fact: '',
         isRandom: false,
         errorMessage: '',
+        days: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        day: '',
+        month: '',
     },
     created: function() {
 
@@ -14,22 +18,23 @@ var app = new Vue({
     methods: {
 
         async submitRequest() {
+            let isDate = (this.selection === 'date');
             this.fact = '';
-            if(this.selection === "") {
+            if(this.selection === "")
                 this.errorMessage = "Please select a category above";
-            }
-            else if(this.numInput === '' && !this.isRandom) {
+            else if((this.day === '' || this.month === '') && isDate && !this.isRandom)
+                this.errorMessage = "Please enter a date above"
+            else if(this.numInput === '' && !isDate && !this.isRandom)
                 this.errorMessage = "Please enter a number above";
-            }
             else {
                 this.errorMessage = '';
                 try {
                     console.log("submitting request...");
-                    const response = await axios.get(this.url + "api?args=" + (this.isRandom ? "random" : this.numInput ) + "/" + this.selection);
+                    const response = await axios.get(this.url + "api?args=" + (this.isRandom ? "random" : ( isDate ? this.month + "/" + this.day : this.numInput) ) + "/" + this.selection);
                     console.log(response);
                     this.fact = response.data.text;
                 } catch (error) {
-                    this.errorMessage = "Make sure to enter a valid number";
+                    this.errorMessage = "Make sure to enter a valid " + ( isDate ? "date" : "number" );
                     console.log(error);
                 }
             }
